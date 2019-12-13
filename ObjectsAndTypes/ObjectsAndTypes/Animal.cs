4e4e13775cde95a6bdfdda4b730f96cfbf0e485a
Taras Triska <taras.triska@gmail.com>
2019-12-13 09:14:17 +0200

    Objects and Types

---
+++ ObjectsAndTypes/ObjectsAndTypes/Animal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ObjectsAndTypes
+{
+    class Animal
+    {
+        private readonly int age;
+        private string mood;
+
+        private bool isHealthy;
+
+        // Animal is constantly not a human.
+        public const bool IsHuman = false;
+
+        public string Name;
+
+        // Property with field.
+        public bool IsHealthy
+        {
+            get => isHealthy;
+            set
+            {
+                isHealthy = value;
+                if (!isHealthy)
+                {
+                    Console.WriteLine($"You need to go to vet with {Name}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the type of the animal.
+        /// </summary>
+        /// <value>
+        /// The type of the animal.
+        /// </value>
+        public AnymalType AnimalType { get; set; }
+
+        // Readonly auto property. Our animal is always hungry if it's age is less than 3.
+        public bool IsHungry => age < 3;
+
+        public Animal(int age)
+        {
+            this.age = age;
+        }
+
+        public void Voice()
+        {
+            Console.WriteLine("Voice");
+            
+            // Can not change readonly.
+            // age = 2;
+
+            mood = "Good";
+            Console.WriteLine($"Animal {Name} is in {mood} mood.");
+        }
+    }
+}
---
+++ ObjectsAndTypes/ObjectsAndTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ObjectsAndTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animal = new Animal(1);
+            animal.Name = "Bob";
+            animal.AnimalType = AnymalType.Dog;
+
+            animal.Voice();
+            animal.IsHealthy = false;
+
+            Console.WriteLine($"Is our animal hungry? - {animal.IsHungry}");
+
+            var anonymous = new
+            {
+                Occupation = "hacker",
+                NickName = "007"
+            };
+
+            Console.WriteLine($"Anonymous is {anonymous}");
+        }
+    }
+}
