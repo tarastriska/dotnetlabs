069b13d358e9da5d40440efc71944b15ef31e9ca
Oleh Shostkevych <osh@e-boks.dk>
2020-01-24 15:49:36 +0200

    Initial

---
+++ Examples/ExplodingKitten/ExplodingKitten/Kitten.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using Figgle;
+namespace ExplodingKitten
+{
+    public class Kitten
+    {
+        public Kitten()
+        {
+           
+        }
+
+        public string getCat(string command)
+        {
+            string phrase = @"     \`   " + command + "'/";
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(@"      /\___/\");
+            sb.AppendLine(@"     /       \");
+            sb.AppendLine(@"    / <|> <|> \");
+            sb.AppendLine(@"    \    *    / ");
+            sb.AppendLine(phrase);
+            sb.AppendLine(@"      `-----'");
+            return sb.ToString();
+        }
+
+        public void sayHi()
+        {
+            Console.WriteLine(this.getCat("Hi"));
+        }
+
+        public void describePerson(string person)
+        {
+            switch (person)
+            {
+                case "lex":
+                    Console.WriteLine(this.getCat("vorovskoy"));
+                    break;
+                case "oleh":
+                    Console.WriteLine(this.getCat("rozbiynuk"));
+                    break;
+                default:
+                    Console.WriteLine(this.getCat("Don't care"));
+                    break;
+            }
+        }
+    }
+}
---
+++ Examples/ExplodingKitten/ExplodingKitten/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ExplodingKitten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kitten = new Kitten();
+            kitten.sayHi();
+            Console.WriteLine("Ask for something ?");
+            var a = Console.ReadLine();
+            while(a != "exit")
+            {
+                kitten.describePerson(a);
+                Console.WriteLine("Ask for something ?");
+                a = Console.ReadLine();
+            }
+        }
+    }
+}
